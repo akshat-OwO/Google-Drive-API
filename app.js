cd73657270178832c347d296d050739dda33426d
Akshat Singh <akshatsinghdelhi@gmail.com>
2022-12-24 23:36:05 +0530

    maths id update

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const drive = google.drive({
 //obj of all the subs and their IDs
 const subs = {
     appliedPhysics1: '1ZlXeO_Y5xuNvGs28QkCY9-ei2TQUXAKz',
-    appliedMaths1: '1bTDDFPpAigGkkg7kL5zrbPQ6veLqJpfL',
+    appliedMaths1: '1OJKah6B3-rtjgdEyhjehSPg5jXlm-HA9',
     appliedChemistry: '12HFIwUareqq4nXdAl5LU599NAbNE18xe',
     communicationSkills: '1GYT9LnArvlcE7qgZ7ryv4WZjFvCOkatI',
     electricalScience: '1OjkRHa6TwYN_ZZW1ZIzfd5cL_zOcm9jT',
