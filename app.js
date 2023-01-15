117deb5b1b95034f1de20a8516b33964354a410b
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-01-15 10:10:51 +0530

    CORS fix

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const { nextTick } = require('process');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://syllabusx.live'
+}));
 
 app.use('/', (req, res, next) => {
     console.log(req.path, req.method);
